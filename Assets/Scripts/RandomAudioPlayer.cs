13e8c1425ab4e80265bc9e4e556e86095da71a0a
NaotoTateno <>
2019-01-31 18:22:00 +0900

    ランダムにBGMを再生することはできた

---
+++ Assets/Scripts/RandomAudioPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 複数の「AudioSourceを持つゲームオブジェクト」からランダムに選択し、音を鳴らす
+public class RandomAudioPlayer : MonoBehaviour
+{
+    private GameObject[] audioObjects;
+
+    private void Start()
+    {
+        // タグでAudioSourceを持つゲームオブジェクト(複数)を見つける
+        audioObjects = GameObject.FindGameObjectsWithTag("AudioObject");
+  
+        // ランダムにオブジェクトを選択
+            var audioObject = audioObjects[Random.Range(0, audioObjects.Length)];
+
+            // AudioSourceをPlay
+            audioObject.GetComponent<AudioSource>().Play();
+
+            // AudioSourceを持つゲームオブジェクトにアタッチしておいたスクリプトからIDを取得する
+            var audioSourceId = audioObject.GetComponent<AudioSourceID>().audioSourceID;
+            print(audioSourceId);
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RandumAudioPlayer
 for Leaning Project
+
+This Project was builded by Unity2017.4.3f1.
